42e4efc7f29593da5b8aaeef20acc007a096b076
Ande Praveen <praveenande84@gmail.com>
2023-09-21 23:35:43 +0530

    Initial Commit

---
+++ 02 Elements/02 Empty Tags/01 Void Elements/04 Form Elements/01 Input Elements/01 text & password/textbox & password/App.js
@@ -0,0 +1,11 @@
+// DOM properties
+
+// value property to get the value of the HTML input Element.
+
+let usernameElement = document.getElementById("username");
+
+// value of the input
+console.log(usernameElement.value);
+
+
+console.log("hello world")
